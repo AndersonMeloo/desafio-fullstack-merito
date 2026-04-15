2b0e78b91083c4ee154f4397f362a0a0f99f647f
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2026-04-14 23:46:49 -0300

    feat(Wallet Service): Implementação: - Busca todos os fundos com as transações - Cálculo de Posisão por Fundo (Aporte e Resgate) - Cálculo de cotas (Sahres e saldo Amount) - Filtragem com Saldo Positivo

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -1,26 +1,63 @@
-import { Injectable } from '@nestjs/common';
-import { CreateWalletDto } from './dto/create-wallet.dto';
-import { UpdateWalletDto } from './dto/update-wallet.dto';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { TransactionType } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class WalletService {
-  create(createWalletDto: CreateWalletDto) {
-    return 'This action adds a new wallet';
-  }
+  constructor(private readonly prisma: PrismaService) {}
 
-  findAll() {
-    return `This action returns all wallet`;
-  }
+  async getSummary() {
+    try {
+      // Busca todos os fundos com as transações
+      const funds = await this.prisma.fund.findMany({
+        include: {
+          transactions: true,
+        },
+      });
 
-  findOne(id: number) {
-    return `This action returns a #${id} wallet`;
-  }
+      // Calcula posição de cada fundo SALDO E COTAS
+      const positions = funds.map((fund) => {
+        const summary = fund.transactions.reduce(
+          (acc, transaction) => {
+            const signal = transaction.type === TransactionType.APORTE ? 1 : -1;
+            acc.amount += signal * transaction.amount;
+            acc.shares += signal * transaction.shares;
+            return acc;
+          },
+          { amount: 0, shares: 0 },
+        );
 
-  update(id: number, updateWalletDto: UpdateWalletDto) {
-    return `This action updates a #${id} wallet`;
-  }
+        return {
+          fundId: fund.id,
+          fundName: fund.name,
+          ticker: fund.ticker,
+          shares: Number(summary.shares.toFixed(6)),
+          balance: Number(summary.amount.toFixed(2)),
+        };
+      });
 
-  remove(id: number) {
-    return `This action removes a #${id} wallet`;
+      // Remove fundos sem posição (ZERADOS OU NEGATIVAS)
+      const filterdPositions = positions.filter(
+        (position) => position.shares > 0,
+      );
+
+      // Soma o valor TOTAL DA CARTEIRA
+      const totalBalance = filterdPositions.reduce(
+        (acc, position) => acc + position.balance,
+        0,
+      );
+
+      return {
+        message: 'Resumo da carteira retornada com sucesso',
+        data: {
+          totalBalance: Number(totalBalance.toFixed(2)),
+          positions: filterdPositions,
+        },
+      };
+    } catch {
+      throw new InternalServerErrorException(
+        'Erro ao calcular o resumo da carteira',
+      );
+    }
   }
 }
